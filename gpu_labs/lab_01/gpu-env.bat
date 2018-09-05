89f443244061b712160fe148385e877138c4b9ea
kevtr0n <kevphayden@gmail.com>
2018-09-04 20:19:30 -0400

    Adding executable files built with nvcc compiler

---
+++ gpu_labs/lab_01/gpu-env.bat
@@ -0,0 +1,6 @@
+rem c:\Program Files (x86)\Microsoft Visual Studio/2017/Community/VC/Tools/MSVC/14.15.26726/bin/Hostx64/x64/cl.exe
+rem c:\Program Files (x86)\Microsoft Visual Studio/2017/Community/VC/Tools/MSVC/14.15.26726/bin/Hostx64/x86/cl.exe
+rem c:\Program Files (x86)\Microsoft Visual Studio/2017/Community/VC/Tools/MSVC/14.15.26726/bin/Hostx86/x64/cl.exe
+rem c:\Program Files (x86)\Microsoft Visual Studio/2017/Community/VC/Tools/MSVC/14.15.26726/bin/Hostx86/x86/cl.exe
+
+set PATH=C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Tools\MSVC\14.15.26726\bin\Hostx64\x64;%PATH%
