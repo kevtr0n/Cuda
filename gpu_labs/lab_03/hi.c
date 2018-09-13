8bde5b8ad7f1f714e48811a97bd4df5ed8d7057e
kevtr0n <kevphayden@gmail.com>
2018-09-13 00:07:17 -0400

    Completed C program that computes the Heat Index. Also included is outputted chart in text file.

--- gpu_labs/lab_03/hi.c
+++ gpu_labs/lab_03/hi.c
@@ -14,14 +14,48 @@
 
 // Prototypes:
 int calculateHeatIndex(float t, float r);
+void generateChart();
 
 // Main Method:
 int main(int argc, char const *argv[]) {
+    
+    generateChart();
     return 0;
 }
 
-// Functions:
+/**
+ * Function: calculateHeatIndex
+ * ----------------------------
+ * Computes the heat index given a dry temp and a humidity value.
+ * 
+ * @param t: The dry bulb temperature.
+ * @param r: The humidity in the form of a whole number percentage.
+ * 
+ * @returns: The heat index rounded to the nearest integer.
+ */
 int calculateHeatIndex(float t, float r) {
     double hi = c1 + (c2 * t) + (c3 * r) + (c4 * t * r) + (c5 * (t * t)) + (c6 * (r * r)) + (c7 * (t * t) * r) + (c8 * t * (r * r)) + (c9 * (t * t) * (r * r));
     return (int) (hi + 0.5);
+}
+
+/**
+ * Function: generateChart
+ * ----------------------------
+ * Prints the formatted heat index chart to the console.
+ */
+void generateChart() {
+
+    printf("Heat Index\n%7s", "Rv F>");
+
+    for (int t = 80; t <= 110; t += 2) {
+        printf("%6d", t);
+    }
+
+    for (int r = 40; r <= 100; r += 5) {
+        printf("\n%6d%%", r);
+        for (int t = 80; t <= 110; t += 2) {
+            int index = calculateHeatIndex((int) t, (int) r);
+            printf("%6d", index);
+        }
+    }
 }
